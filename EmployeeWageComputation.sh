9a04103862bae68dc5c9273f4db3988b6786b2e3
patelashok <ashok.patel2917@gmail.com>
2021-05-01 16:33:47 +0530

    ADDED:Calculate total wage till working days emphrs per month reached

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,21 +4,27 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 WAGE_PER_HR=20
 MAX_WORKING_DAYS=20
-
+MAX_HRS_IN_MONTH=100
 #variable
-for((counter=1;counter<=$MAX_WORKING_DAYS;counter++))
+totalWorkinghours=0
+totalWorkingdays=0
+while [[ $totalWorkinghours -lt $MAX_HRS_IN_MONTH &&
+        $totalWorkingdays -lt $MAX_WORKING_DAYS ]]
 do
         employeecheck=$((RANDOM%3))
+        ((totalWorkingdays++))
         case $employeecheck in
                 $IS_PRESENT_FULL_TIME)
-                        emphrs=12;;
-                $IS_PRESENT_PART_TIME)
                         emphrs=8;;
+                $IS_PRESENT_PART_TIME)
+                        emphrs=4;;
                 *)
                         emphrs=0;;
         esac
+        totalWorkinghours=$(($totalWorkinghours + $emphrs))
 done
-salary=$(( $emphrs * $WAGE_PER_HR ))
+
+salary=$(( $totalWorkinghours * $WAGE_PER_HR ))
 
 
 
