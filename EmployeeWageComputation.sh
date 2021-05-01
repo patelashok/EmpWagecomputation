8392f6cdaefce019de8a51a1c4f769b5fffdf3d1
patelashok <ashok.patel2917@gmail.com>
2021-05-01 14:30:29 +0530

    ADDED :part time emp & wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,17 +1,18 @@
 #! /bin/bash -x
 
 #constants
-IS_PRESENT=1
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 
 #variable
-empCheck=$(( RANDOM % 2))
+empCheck=$(( RANDOM % 3))
  #SELECTION
-if [  $empCheck -eq $IS_PRESENT ]
-then
-        emphrs=8
-else
-        emphrs=0
-fi
+case $empCheck in
+        $IS_PRESENT_FULL_TIME ) emphrs=12;;
+        $IS_PRESENT_PART_TIME ) emphrs=8;;
+        *) emphrs=0;;
+esac
 salary=$(( EMP_RATE_PER_HR * emphrs ))
+
 
