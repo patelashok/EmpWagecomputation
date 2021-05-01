783ec015082fa80ef0f674d6fd4f6102ad00065c
patelashok <ashok.patel2917@gmail.com>
2021-05-01 17:27:54 +0530

    ADDED store the Day and Daily wage along with total wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,44 +1,46 @@
 #! /bin/bash -x
-#constrants for the program
-IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=4;
+MAX_HRS_IN_MONTH=80;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-#variables
-totalEmpHrs=0;
+#VARIABLES
+ totalEmpHr=0;
 totalWorkingDays=0;
 
-function getWorksHrs(){
-        local $empCheck=$1
-        case $empCheck in
-                $IS_FULL_TIME)
-                        empHrs=8;;
-                $IS_PART_TIME)
-                        empHrs=4;;
-                                                *)
-                        empHrs=0;;
-        esac
-        echo $empHrs
+declare -A dailyWage
+
+function getWorkHrs(){
+local empCheck=$1;
+case $empCheck in $IS_FULL_TIME)
+empHrs=8 ;;
+$IS_PART_TIME)empHrs=4 ;;
+*)empHrs=0;;
+
+esac
+echo $empHrs
+
 }
 
-function getEmpWage()
-{
-        local empHr=$1
-        echo $(($empHr*$EMP_RATE_PER_HR))
+function getEmpWage() {
+local empHr=$1
+echo $(($empHr*$EMP_RATE_PER_HR))
+
 }
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
-                   $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+while [[  $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
+ $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
 do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3));
-        empHrs="$( getWorksHrs $empCheck )"
-        totalEmpHrs=$(($totalEmpHrs+$empHrs))
-        dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
+
+
+ ((totalWorkingDays++))
+ empCheck=$((RANDAM%3));
+ empHrs="$( getWorkHrs $empCheck )"
+ totalEmpHrs=$(($totalEmpHr+$empHrs))
+dailyWage["Day $totalWorkingDays"]="$( getEmpWage $empHrs )"
+
 done
-
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
-echo ${dailyWage[@]}
-
+ totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+echo  ${dailyWage[@]}
+echo ${!dailyWage[@]}
